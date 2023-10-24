45b418fe5df871c2b34afe1e90e7e537636848c6
Kajanan Chinniah <kajananchinniah@gmail.com>
2023-10-23 23:42:10 -0400

    changed eigen tag to 3.4 due to incompataibility I can't figure out

--- external/eigen.cmake
+++ external/eigen.cmake
@@ -2,12 +2,12 @@ include(FetchContent)
 FetchContent_Declare(
   Eigen
   GIT_REPOSITORY https://gitlab.com/libeigen/eigen.git
-  GIT_TAG master
+  GIT_TAG 3.4
   GIT_SHALLOW TRUE
   GIT_PROGRESS TRUE)
 
 # Don't build off docs, tests, etc
-set(EIGEN_BUILD_TESTING OFF)
+set(BUILD_TESTING OFF)
 set(EIGEN_BUILD_DOC OFF)
 
 FetchContent_MakeAvailable(Eigen)
